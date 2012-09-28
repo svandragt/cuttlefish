462ddcab8257c3553edbbd9fe36ed8df6edaf0a5
Sander van Dragt <sander.vandragt@gmail.com>
2012-09-28 13:52:47 +0100

    disable caching by default

--- public/Configuration.php
+++ public/Configuration.php
@@ -3,6 +3,6 @@
 class Configuration extends Defaults {
 		const SITE_TITLE = 'Carbon';
 		const SITE_MOTTO = 'Home of Carbon, the hackable performant PHP based static blogging system.';
-		const CACHE_ENABLED  = true;
+		const CACHE_ENABLED  = false;
 
 }
