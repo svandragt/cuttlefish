f860c9054c3c680a926e2dcc29f528ee54dd483a
Sander van Dragt <sander.vandragt@gmail.com>
2012-09-28 14:10:32 +0100

    logging when files can't be found

--- public/system/Model.php
+++ public/system/Model.php
@@ -5,6 +5,7 @@ class Model {
 	static function load_data($filename) {
 		if (is_null($filename)) return null;
 		if (!file_exists ($filename )) {
+			Log::info("'$filename' cannot be found.");
 			header('Location: ' . Theming::root() . '/error/404');
 		} else {
 			$contents =  file_get_contents ( $filename);
