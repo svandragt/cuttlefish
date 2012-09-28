eb320eca9c8125051becb2cacb64f01c6ff323df
Sander van Dragt <sander.vandragt@gmail.com>
2012-09-28 13:18:34 +0100

    Updated basic configuration

--- public/Configuration.php
+++ public/Configuration.php
@@ -1,8 +1,8 @@
 <?php
 
 class Configuration extends Defaults {
-		const SITE_TITLE = 'MDBlog';
-		const SITE_MOTTO = 'Home of MDBlog';
+		const SITE_TITLE = 'Carbon';
+		const SITE_MOTTO = 'Home of Carbon, the hackable performant PHP based static blogging system.';
 		const CACHE_ENABLED  = true;
 
 }
