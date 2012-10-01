d9d3756933be9758398c5b697b80517941bf2f2c
Sander van Dragt <sander.vandragt@gmail.com>
2012-10-01 14:08:04 +0100

    data models as static class variables

--- public/system/Model.php
+++ public/system/Model.php
@@ -2,15 +2,22 @@
 
 class Model {
 
+	public static $page_model = array(
+		'markdown|html' => 'content',
+	);
+
+	public static $post_model = array(
+		'yaml' => 'metadata',
+		'markdown|html' => 'content',
+	);
+
 	static function error($file_path) {
 		return self::pages($file_path);
 	}
 
 	static function pages($file_path) {
 		if (Filesystem::is_found($file_path)) {
-			$model = new Datamodel($file_path, array(
-				'markdown|html' => 'content',
-			));
+			$model = new Datamodel($file_path, self::$page_model);
 			return $model;
 		} else {
 			header('Location: ' . Theming::root() . '/error/404');
@@ -22,10 +29,7 @@ class Model {
 
 	static function posts($file_path) {
 		if (Filesystem::is_found($file_path)) {
-			$model = new Datamodel($file_path, array(
-				'yaml' => 'metadata',
-				'markdown|html' => 'content',
-			));
+			$model = new Datamodel($file_path, self::$post_model);
 			return $model;
 		} else {
 			header('Location: ' . Theming::root() . '/error/404');
