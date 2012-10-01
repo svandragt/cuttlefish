eb7df286fbe9bea4a4754649d888d5418e4170e0
Sander van Dragt <sander.vandragt@gmail.com>
2012-10-01 11:37:59 +0100

    Reworked model. Specify the section types and datamodel fields in the model and Carbon will wire up the datamodel for you.

---
+++ public/system/Datamodel.php
@@ -0,0 +1,47 @@
+<?php
+
+class Datamodel {
+		function __construct($file_path, $section_types) {
+
+    	if (array_unique ($section_types) !== $section_types) throw new Exception('Array values not unique');
+
+        // Call the Model constructor
+   		$this->file_path = $file_path;
+		$this->link      = Theming::content_url($file_path);
+
+		$this->sections = preg_split( '/\R\R/',  trim(file_get_contents($this->file_path)), 2);
+		$section_keys   = array_keys($section_types);
+		$section_values = array_values($section_types);
+
+		if (count($this->sections) != count($section_keys)) throw new Exception('Model definition does not match number of content sections.');
+
+		$mdep = null;
+		$spyc = null;
+		if (Ext::class_loaded( 'MarkdownExtra_Parser')) $mdep = new MarkdownExtra_Parser;
+		if (Ext::class_loaded( 'Spyc')) $spyc = new Spyc;
+
+		for ($i = 0; $i < count($section_types); $i++) { 
+			$section_key   = $section_keys[$i];
+			$section_value = $section_values[$i];
+			switch ($section_key) {
+				case 'yaml':
+					if ($spyc) $this->$section_value =  $spyc->YAMLLoadString($this->sections[$i]);
+					else $this->$section_value =  $this->sections[$i];
+					break;
+				case 'markdown':
+					$content_sections = preg_split( '/=\R/',  trim($this->sections[$i]), 2);	
+					$title_sections   = preg_split( '/\R/',  trim($content_sections[0]), 2);	
+					$this->title      = $title_sections[0];
+
+					if ($mdep) $this->$section_value =  $mdep->transform($content_sections[1]);
+					else $this->$section_value =  $content_sections[1];
+					break;
+				
+				default:
+					# code...
+					break;
+			}
+			# code...
+		}
+    }
+}
--- public/system/Model.php
+++ public/system/Model.php
@@ -1,67 +1,35 @@
 <?php
 
 class Model {
-	const METADATA = 0;
-	const CONTENT = 1;
-	const TOTAL_SECTIONS = 2;
 
-	static function error($filename) {
-		return self::pages($filename);
+	static function error($file_path) {
+		return self::pages($file_path);
 	}
 
-	static function pages($filename) {
-		if (is_null($filename)) return null;
-		if (!file_exists ($filename )) {
-			Log::info("'$filename' cannot be found.");
-			header('Location: ' . Theming::root() . '/error/404');
-		} else {
-			$content = trim(file_get_contents($filename));
-			
-			$model = new stdClass();
-			if (Ext::class_loaded( 'MarkdownExtra_Parser')){
-				$c = new MarkdownExtra_Parser;
-				$model->content =  $c->transform($content);
-			}
-			else {
-				$model->content =  $content;
-			}
+	static function pages($file_path) {
+		if (Filesystem::is_found($file_path)) {
+			$model = new Datamodel($file_path, array(
+				'markdown' => 'content',
+			));
 			return $model;
+		} else {
+			header('Location: ' . Theming::root() . '/error/404');
+			exit();
 		}
 	}
 
 		
 
 	static function posts($file_path) {
-		if (is_null($file_path)) return null;
-		if (!file_exists ($file_path )) {
-			Log::info("'$file_path' cannot be found.");
-			header('Location: ' . Theming::root() . '/error/404');
-		} else {
-			$model            = new stdClass();
-			$model->file_path = $file_path;
-			$model->link      = Theming::content_url($file_path);
-
-			$segments         = preg_split( '/\R\R/',  trim(file_get_contents($model->file_path)), self::TOTAL_SECTIONS);
-			$content_segments = preg_split( '/=\R/',  trim($segments[self::CONTENT]), 2);	
-			$title_segments   = preg_split( '/\R/',  trim($segments[self::CONTENT]), 2);	
-			$model->title     = $title_segments[0];
-
-			if (isset($segments[self::METADATA])) if (Ext::class_loaded( 'Spyc')){
-				$c = new Spyc;
-				$model->meta_data =  $c->YAMLLoadString($segments[self::METADATA]);
-			}
-			if (isset($segments[self::CONTENT])) {
-				if (Ext::class_loaded( 'MarkdownExtra_Parser')){
-					$c = new MarkdownExtra_Parser;
-					$model->content =  $c->transform($content_segments[1]);
-				}
-				else {
-					// fallback
-					$model->content =  $content_segments[1];
-				}
-			}
-
+		if (Filesystem::is_found($file_path)) {
+			$model = new Datamodel($file_path, array(
+				'yaml' => 'metadata',
+				'markdown' => 'content',
+			));
 			return $model;
-		}
+		} else {
+			header('Location: ' . Theming::root() . '/error/404');
+			exit();
+		}		
 	}
 }
