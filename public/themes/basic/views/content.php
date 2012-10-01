00f63fecf5ae8663a26e07d68c69f6c8eb8744bb
Sander van Dragt <sander.vandragt@gmail.com>
2012-10-01 12:54:35 +0100

    Content view differentiation depending on loop

--- public/themes/basic/views/content.php
+++ public/themes/basic/views/content.php
@@ -1,9 +1,14 @@
 <?php  if ( ! defined('THEME_DIR')) exit('No direct script access allowed'); 
 echo PHP_EOL;
-
-foreach ($this->models as $model) {
+if ( count($this->models) > 1) foreach ($this->models as $model) {
 	printf("<article>
 		<h2><a href='%s'>%s</a></h2>
 		%s
 		</article>", $model->link, $model->title, $model->content);
+} else {
+	$model = $this->models[0];
+	printf("<article>
+		<h2>%s</h2>
+		%s
+		</article>", $model->title,$model->content);
 }
