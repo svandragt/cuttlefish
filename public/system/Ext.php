2ce9acb14ca8e2ee4d6f481526b1cae775cccbc4
Sander van Dragt <sander.vandragt@gmail.com>
2012-10-01 14:29:29 +0100

    Move add_include_path to setup class (and add application folder classes

--- public/system/Ext.php
+++ public/system/Ext.php
@@ -7,12 +7,8 @@ class Ext {
 	static function environment_start() {
 		foreach (Filesystem::list_files( Filesystem::url_to_path('/system/Ext'), 'php') as $key => $value) {
 			self::$register[pathinfo($value,PATHINFO_FILENAME)] = true;
-			self::add_include_path(pathinfo($value,PATHINFO_DIRNAME));	
+			Setup::add_include_path(pathinfo($value,PATHINFO_DIRNAME));	
 		}
-	}
-
-	static function add_include_path($path) {
-		set_include_path(get_include_path() . PATH_SEPARATOR . realpath($path));
 	}
 
 	static function class_loaded($classname) {
--- public/system/Setup.php
+++ public/system/Setup.php
@@ -3,12 +3,14 @@
 class Setup {
 
 	static public function environment_start() {
+		self::add_include_path(Filesystem::url_to_path('/'.Configuration::APPLICATION_FOLDER));	
 		define('THEME_DIR', DIRECTORY_SEPARATOR . Configuration::THEMES_FOLDER . DIRECTORY_SEPARATOR . Configuration::THEME . DIRECTORY_SEPARATOR);
 		if ( self::new_install()) {
 			Filesystem::ensure_folder_exists( Configuration::LOGS_FOLDER );
 			Filesystem::ensure_folder_exists( Configuration::CACHE_FOLDER );
 			Filesystem::ensure_folder_exists( Configuration::CONTENT_FOLDER . '/pages');
 			Filesystem::ensure_folder_exists( Configuration::CONTENT_FOLDER . '/posts');
+			Filesystem::ensure_folder_exists( Configuration::CONTENT_FOLDER . '/errors');
 			Filesystem::ensure_folder_exists( Configuration::THEMES_FOLDER);
 		}
 
@@ -21,4 +23,9 @@ class Setup {
 	static private function new_install() {
 		return !(is_dir( Configuration::CACHE_FOLDER ) && is_dir( Configuration::CONTENT_FOLDER )) ;
 	}
+
+	static function add_include_path($path) {
+		set_include_path(get_include_path() . PATH_SEPARATOR . realpath($path));
+	}
+
 }
