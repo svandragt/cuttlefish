091b92b83b7c6149f165970b42567b069d8ef5fa
Sander van Dragt <sander.vandragt@gmail.com>
2012-10-01 14:29:02 +0100

    add application folder configurable

--- public/system/Defaults.php
+++ public/system/Defaults.php
@@ -2,18 +2,19 @@
 
 class Defaults {
 
-	const SITE_TITLE     = 'Your site';
 	const SITE_MOTTO     = 'Something clever for the internets';
-	const HOME_PAGE      = '/index';
-
-	const INDEX_PAGE     = 'index.php';
-	const CACHE_FOLDER   = '../cache';
-	const CONTENT_FOLDER = 'content';
-	const LOGS_FOLDER    = '../logs';
-	const THEMES_FOLDER  = 'themes';
+	const SITE_TITLE     = 'Your site';
 	const THEME          = 'basic';
-	
+
 	const CACHE_ENABLED  = false;
-	const CONTENT_EXT   = 'md';
+	const CONTENT_EXT    = 'md';
+	const INDEX_PAGE     = 'index.php';
+
+	const HOME_PAGE          = '/index';
+	const APPLICATION_FOLDER = 'application';
+	const CACHE_FOLDER       = '../cache';
+	const CONTENT_FOLDER     = 'content';
+	const LOGS_FOLDER        = '../logs';
+	const THEMES_FOLDER      = 'themes';
 
 }
