cac162c975d7ad9ad1378dad6596eae1ecbaafc4
Sander van Dragt <sander.vandragt@gmail.com>
2012-10-02 11:25:47 +0100

    add handles for controller and model specific styling

--- public/themes/basic/views/content.php
+++ public/themes/basic/views/content.php
@@ -2,8 +2,12 @@
 
 echo PHP_EOL;
 
+printf("<div class='%s %s'>", $this->controller, $this->model);
+
 if ( count($this->models) > 1) {
 	include __DIR__ . DIRECTORY_SEPARATOR . $this->controller . ".php";
 } else {
 	include __DIR__ . DIRECTORY_SEPARATOR . $this->model . ".php";
 }
+
+print("</div>");
