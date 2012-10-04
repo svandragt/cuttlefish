9089c9ea5cc46e4b35b9c13351d62b81144a0dc3
Sander van Dragt <sander.vandragt@gmail.com>
2012-10-04 12:42:12 +0100

    generate appropriate htaccess web.config for static site

--- public/system/Setup.php
+++ public/system/Setup.php
@@ -12,6 +12,7 @@ class Setup {
 			Filesystem::ensure_folder_exists( Configuration::CONTENT_FOLDER . '/posts');
 			Filesystem::ensure_folder_exists( Configuration::CONTENT_FOLDER . '/errors');
 			Filesystem::ensure_folder_exists( Configuration::THEMES_FOLDER);
+			self::webserver();
 		}
 
 		Ext::environment_start();
@@ -27,6 +28,28 @@ class Setup {
 
 	static function add_include_path($path) {
 		set_include_path(get_include_path() . PATH_SEPARATOR . realpath($path));
+	}
+
+	static function webserver() {
+		$directory_index = "index.html index.xml";
+		$path = Configuration::CACHE_FOLDER . DIRECTORY_SEPARATOR . ".htaccess";
+		$fp = fopen($path, 'w'); 
+		fwrite($fp, "DirectoryIndex  $directory_index"); 
+		fclose($fp); 
+
+
+		$path = Configuration::CACHE_FOLDER . DIRECTORY_SEPARATOR . "web.config";
+        $xml = new SimpleXMLElement('<configuration></configuration>'); 
+        $sys = $xml->addChild('system.webServer'); 
+        $sys->addChild('defaultDocument'); 
+        $files = $sys->defaultDocument->addChild('files'); 
+        $files->addChild('clear'); 
+        $values = explode(' ', $directory_index);
+        foreach ($values as $value) {
+    		$add = $files->addChild('add'); 
+	        $add->addAttribute('value', $value);
+        }
+        $xml->asXML($path);
 	}
 
 }
