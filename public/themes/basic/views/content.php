e376c558a395a4f575cd432153ba7c2a7fd058c7
Sander van Dragt <sander.vandragt@gmail.com>
2012-10-04 14:57:15 +0100

    always use controller

--- public/themes/basic/views/content.php
+++ public/themes/basic/views/content.php
@@ -8,13 +8,7 @@ switch (count($this->models)) {
 		printf("<p> Please <a href='%s'>add content</a> to /%s/%s.</p>", Url::content_url('/admin/new'), Configuration::CONTENT_FOLDER, $this->model);
 		break;
 	
-	case 1:
-		// single piece of content (ie individual page/post)
-		include __DIR__ . DIRECTORY_SEPARATOR . $this->model . ".php";
-		break;
-	
 	default:
-		// 2 or more pieces of content (loop - ie. index/archive)
 		include __DIR__ . DIRECTORY_SEPARATOR . $this->controller . ".php";
 		break;
 }
