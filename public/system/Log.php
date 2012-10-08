d4fc8108d7805de329e96b5d119930a74e695e7e
Sander van Dragt <sander.vandragt@gmail.com>
2012-10-08 12:56:23 +0100

    one logfile per day, except debug calls who use one per request

--- public/system/Log.php
+++ public/system/Log.php
@@ -2,20 +2,37 @@
 
 class Log {
 
+	const FILENAME_TEMPLATE = '/mdblog-%s.log';
+
 	static function error( $message ) {
 		error_log($message,0);
 		echo($message. "<br>");
 	}
 
 
+	static function debug( $message ) {
+		if (Configuration::DEBUG_ENABLED) {
+			$message = sprintf("[%s] (%s) DEBUG %s", date('d/M/Y:H:i:s'), pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME), $message . PHP_EOL);
+			error_log($message, 3, Configuration::LOGS_FOLDER . self::filename_per_request());
+		}
+	}
+
 	static function info( $message ) {
-		$message = sprintf("[%s] (%s) %s", date('d/M/Y:H:i:s'), pathinfo(Http::server('PHP_SELF'), PATHINFO_FILENAME), $message . PHP_EOL);
-		error_log($message, 3, Configuration::LOGS_FOLDER . '/mdblog.log');
+		$message = sprintf("[%s] (%s) INFO %s", date('d/M/Y:H:i:s'), pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME), $message . PHP_EOL);
+		error_log($message, 3, Configuration::LOGS_FOLDER . self::filename());
 	}
 
 	static function warn( $message ) {
-		$message = sprintf("[%s] (%s) WARN %s", date('d/M/Y:H:i:s'), pathinfo(Http::server('PHP_SELF'), PATHINFO_FILENAME), $message . PHP_EOL);
-		error_log($message, 3, Configuration::LOGS_FOLDER . '/mdblog.log');
+		$message = sprintf("[%s] (%s) WARN %s", date('d/M/Y:H:i:s'), pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME), $message . PHP_EOL);
+		error_log($message, 3, Configuration::LOGS_FOLDER . self::filename());
+	}
+
+	static function filename() {
+		return sprintf(self::FILENAME_TEMPLATE, date("Y-m-d"));
+	}
+
+	static function filename_per_request() {
+		return sprintf(self::FILENAME_TEMPLATE, date("Y-m-d_H-m-s"));
 	}
 
 }
