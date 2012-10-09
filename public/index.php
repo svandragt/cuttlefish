717b2d157afd0f64995c61b5019495de797b6f02
Sander van Dragt <sander.vandragt@gmail.com>
2012-10-09 16:09:52 +0100

    Remove profiling as we can't use it on all pages (non-xml)

--- public/index.php
+++ public/index.php
@@ -1,8 +1,4 @@
 <?php
-$then = microtime();
 define('BASEPATH', __DIR__);
 include 'system/__autoload.php';
 Carbon::router();
-
-$now = microtime();
-// echo sprintf("<span class='diagnostics'>Elapsed:  %f ms</span>", $now-$then);
