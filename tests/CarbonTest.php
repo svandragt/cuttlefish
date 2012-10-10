75055779bb686fa818ae2598e2a7e2a7abf9a8d0
Sander van Dragt <sander.vandragt@gmail.com>
2012-10-10 15:55:29 +0100

    irc notification of build

--- tests/CarbonTest.php
+++ tests/CarbonTest.php
@@ -7,24 +7,4 @@ class CarbonTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals('/index', $empty);
 	}
 
-	public function test_has_cacheable_page_request_no_cache() {
-		$this->assertEquals(false, Cache::has_cacheable_page_request());
-	}
-
-	public function test_has_cacheable_page_request_with_cache() {
-		Cache::start();
-		$this->assertEquals(true, Cache::has_cacheable_page_request());
-		Cache::end();
-	}
-
-	public function test_cache_file_from_url_null() {
-		$this->assertEquals(true, Cache::cache_file_from_url());
-	}
-
-	public function test_cache_file_from_url_path_info() {
-		$path = self::cache_file_from_url(Carbon::path_info());
-		$this->assertEquals(true, $path);
-	}
-
-
 }  
