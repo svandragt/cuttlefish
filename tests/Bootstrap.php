2250e814280a511db9520d3385cfac42ee8f612a
Sander van Dragt <sander.vandragt@gmail.com>
2012-10-10 11:04:27 +0100

    bootstrapping

---
+++ tests/Bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL | E_STRICT);
+$dir = realpath(dirname(__FILE__));
+
+defined('BASEPATH') OR define('BASEPATH', realpath($dir.'/../public'));
+
+isset($_SERVER['PATH_INFO']) OR $_SERVER['PATH_INFO'] = '/';
+
+include BASEPATH .'/system/__autoload.php';
