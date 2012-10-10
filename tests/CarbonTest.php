f744e9faaff3a00de61e74dee85be3bfae9fd24e
Sander van Dragt <sander.vandragt@gmail.com>
2012-10-10 11:20:09 +0100

    adjust test for expected output

--- tests/CarbonTest.php
+++ tests/CarbonTest.php
@@ -4,6 +4,6 @@ class CarbonTest extends PHPUnit_Framework_TestCase {
 	
 	public function test_path_info() {
 		$empty = Carbon::path_info();
-		$this->assertEquals(0, $empty);
+		$this->assertEquals('/index', $empty);
 	}
 }
