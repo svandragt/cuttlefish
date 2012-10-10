7a1cfae0c3cda55ca6843a38ac941b92c2051e2e
Sander van Dragt <sander.vandragt@gmail.com>
2012-10-10 11:17:45 +0100

    fix autoloading

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -3,10 +3,17 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL | E_STRICT);
 $dir = realpath(dirname(__FILE__));
 
+spl_autoload_register('carbon_autoloader');
+
+
 defined('BASEPATH') OR define('BASEPATH', realpath($dir.'/../public'));
 
 isset($_SERVER['PATH_INFO']) OR $_SERVER['PATH_INFO'] = '/';
 
 set_include_path(get_include_path() . PATH_SEPARATOR . realpath(BASEPATH));
 
-include BASEPATH .'/system/__autoload.php';
+unset($dir);
+
+function carbon_autoloader($class_name) {
+	require($class_name.'.php');
+}
