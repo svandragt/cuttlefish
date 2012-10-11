2c51a877cd50bc76fc089dc5c5a1b4229269d6eb
Sander van Dragt <sander.vandragt@gmail.com>
2012-10-11 09:27:05 +0100

    check for file before autoloading

--- public/system/autoload.php
+++ public/system/autoload.php
@@ -1,7 +1,10 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); 
 
 function carbon_autoloader($class_name) {
-	require($class_name.'.php');
+	$file = $class_name . '.php';
+	if (file_exists($file)) {
+        require $file;
+    }
 }
 
 spl_autoload_register('carbon_autoloader');
