b0b65d48b0f1e79754d94572d3d3bfb92511767d
Sander van Dragt <sander.vandragt@gmail.com>
2012-10-11 09:32:33 +0100

    try loading actual autoloader into bootstrap

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -3,18 +3,13 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL | E_STRICT);
 $dir = realpath(dirname(__FILE__));
 
-spl_autoload_register('carbon_autoloader');
-
 
 defined('BASEPATH') OR define('BASEPATH', realpath($dir.'/../public'));
 
 isset($_SERVER['PATH_INFO']) OR $_SERVER['PATH_INFO'] = '/';
 
 set_include_path(get_include_path() . PATH_SEPARATOR . realpath(BASEPATH));
-set_include_path(get_include_path() . PATH_SEPARATOR . realpath(BASEPATH. '/system'));
+
+include 'system/autoload.php';
 
 unset($dir);
-
-function carbon_autoloader($class_name) {
-	require($class_name.'.php');
-}
