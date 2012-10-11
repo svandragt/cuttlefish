3fbf165bd42169ebb4977a6e13b93cc30a62524d
Sander van Dragt <sander.vandragt@gmail.com>
2012-10-11 09:43:33 +0100

    cache test improvemnts

--- tests/CacheTest.php
+++ tests/CacheTest.php
@@ -3,21 +3,18 @@
 class CacheTest extends PHPUnit_Framework_TestCase {
 
 
-	public function test_has_cacheable_page_request_no_cache() {
-		$this->assertEquals(false, Cache::has_cacheable_page_request());
-	}
-
-	public function test_has_cacheable_page_request_with_cache() {
+	public function test_has_cacheable_page_request() {
+		$request = Cache::has_cacheable_page_request();
+		$this->assertEquals(false, $request);
 		Cache::start();
-		$this->assertEquals(true, Cache::has_cacheable_page_request());
+		$request = Cache::has_cacheable_page_request();
+		$this->assertEquals(true, $request);
 		Cache::end();
 	}
 
-	public function test_cache_file_from_url_null() {
-		$this->assertEquals(true, Cache::cache_file_from_url());
-	}
-
-	public function test_cache_file_from_url_path_info() {
+	public function test_cache_file_from_url() {
+		$path = Cache::cache_file_from_url();
+		$this->assertEquals(true, $path);
 		$path = Cache::cache_file_from_url('/index');
 		$this->assertEquals(true, $path);
 	}
