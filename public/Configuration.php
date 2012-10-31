dcadfd227737e584ac33e438d2f0b70da1c0f924
Sander van Dragt <sander.vandragt@gmail.com>
2012-10-31 09:47:00 +0000

    Your site is the site

--- public/Configuration.php
+++ public/Configuration.php
@@ -1,7 +1,7 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); 
 
 class Configuration extends Defaults {
-		const SITE_TITLE    = 'Carbon';
+		const SITE_TITLE    = 'Your site';
 		const SITE_MOTTO    = 'Example theme of <a href="http://carbon.vandragt.com/">Carbon</a>, the hackable performant (semi) static blogging system.';
 		const CACHE_ENABLED = false;
 
