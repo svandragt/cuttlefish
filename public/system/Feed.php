26cb9ded2d9455eca6c544c3902f9fbebc0db1cf
Sander van Dragt <sander.vandragt@gmail.com>
2012-11-01 11:43:53 +0000

    links are now correct

--- public/system/Feed.php
+++ public/system/Feed.php
@@ -4,24 +4,23 @@ class Feed {
 	
 	function __construct($posts)
 	{
+        $page_url = new Url(); 
         $xml = new SimpleXMLElement('<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom"></rss>'); 
         $xml->addChild('channel'); 
         $xml->channel->addChild('title', Configuration::SITE_TITLE); 
-        $xml->channel->addChild('link', $_SERVER['PATH_INFO']); 
+        $xml->channel->addChild('link', $page_url->index($_SERVER['PATH_INFO'])->abs()->url); 
         $xml->channel->addChild('description', strip_tags(Configuration::SITE_MOTTO)); 
         $xml->channel->addChild('pubDate', date(DATE_RSS)); 
         $atom = $xml->channel->addChild('link','','http://www.w3.org/2005/Atom');
-        $atom->addAttribute('href', $_SERVER['PATH_INFO'] );
+        $atom->addAttribute('href', $page_url->url);
         $atom->addAttribute('rel', 'self');
         $atom->addAttribute('type','application/rss+xml');
 
         foreach ($posts as $post) {
-            $url = new Url();
-            $url = $url->index($post->link)->abs()->url;
             $item = $xml->channel->addChild('item'); 
             $item->addChild('title', $post->content->title); 
-            $item->addChild('link', $url); 
-            $item->addChild('guid', $url);  
+            $item->addChild('link', $post->link); 
+            $item->addChild('guid', $post->link);  
             $item->addChild('description', $post->content->main); 
             $item->addChild('pubDate', date(DATE_RSS, strtotime($post->metadata->Published))); 
         }
