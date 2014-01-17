200a0d3238a0b2ee204747a56308e0a27d86274e
Sander van Dragt <sander@vandragt.com>
2014-01-17 18:08:32 +0000

    fix: set permissions on the cache folder.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ rm -rf /var/www
 ln -fs /vagrant/public /var/www
 mkdir /vagrant/cache
 chown :www-data /vagrant/cache -R
-chmog g+w /vagrant/cache -R
+chmod g+w /vagrant/cache -R
 sed -i '/AllowOverride None/c AllowOverride All' /etc/apache2/sites-available/default
 a2enmod rewrite
 service apache2 reload
