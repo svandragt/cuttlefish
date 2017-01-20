e13453600abff589d091475c6205c25ff9b4af83
Sander van Dragt <sander@vandragt.com>
2017-01-20 08:17:35 +0000

    #26 replaced autoloader with Composer

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 define('BASE_FILEPATH', str_replace('index.php', '', $_SERVER['SCRIPT_FILENAME']) . '/');
 define('BASE_PATH', str_replace('index.php', '', $_SERVER['SCRIPT_NAME']));
-include 'system/autoload.php';
+$loader = require '../vendor/autoload.php';
+$loader->add('', __DIR__.'/');
+$loader->add('', __DIR__.'/system/');
 $carbon = new Request();
