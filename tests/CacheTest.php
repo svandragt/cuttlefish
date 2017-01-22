fd9415999a79c0ba89ce46b9772fbd7c475fda61
Sander van Dragt <sander@vandragt.com>
2017-01-22 01:32:26 +0000

    Yesss... now just have working tests (it's been so long)

--- tests/CacheTest.php
+++ tests/CacheTest.php
@@ -6,18 +6,21 @@ class CacheTest extends PHPUnit_Framework_TestCase {
 
 
 	public function test_has_cacheable_page_request() {
-		$request = Carbon\Cache::has_cacheable_page_request();
+		chdir($this->cwd); // Not a bug (LOL): https://bugs.php.net/bug.php?id=30210
+		$cache = new Carbon\Cache();
+		$request = $cache->has_cacheable_page_request();
 		$this->assertEquals(false, $request);
-		Carbon\Cache::start();
-		$request = Carbon\Cache::has_cacheable_page_request();
+		$cache->start();
+		$request = $cache->has_cacheable_page_request();
 		$this->assertEquals(true, $request);
-		Carbon\Cache::end();
+		$cache->end();
 	}
 
 	public function test_cache_file_from_url() {
-		$path = Carbon\Cache::cache_file_from_url();
+		$cache = new Carbon\Cache();
+		$path = $cache->cache_file_from_url();
 		$this->assertEquals(true, $path);
-		$path = Carbon\Cache::cache_file_from_url('/index');
+		$path = $cache->cache_file_from_url('/index');
 		$this->assertEquals(true, $path);
 	}
 
