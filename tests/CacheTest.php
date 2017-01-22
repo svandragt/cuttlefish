793da95cbe981a2a28a076eb81b0009dae833efc
Sander van Dragt <sander@vandragt.com>
2017-01-22 01:35:36 +0000

    again

--- tests/CacheTest.php
+++ tests/CacheTest.php
@@ -7,7 +7,7 @@ class CacheTest extends PHPUnit_Framework_TestCase {
 
 	public function test_has_cacheable_page_request() {
 		chdir($this->cwd); // Not a bug (LOL): https://bugs.php.net/bug.php?id=30210
-		$cache = new Carbon\Cache();
+		$cache = new Carbon\Cache($this);
 		$request = $cache->has_cacheable_page_request();
 		$this->assertEquals(false, $request);
 		$cache->start();
@@ -17,7 +17,7 @@ class CacheTest extends PHPUnit_Framework_TestCase {
 	}
 
 	public function test_cache_file_from_url() {
-		$cache = new Carbon\Cache();
+		$cache = new Carbon\Cache($this);
 		$path = $cache->cache_file_from_url();
 		$this->assertEquals(true, $path);
 		$path = $cache->cache_file_from_url('/index');
