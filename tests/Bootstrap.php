3ee391bd129b4b9b95c83aff02ff92090b99b640
Sander van Dragt <sander@vandragt.com>
2017-01-22 01:07:25 +0000

    possbile fix tests that run now yay

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -11,6 +11,6 @@ isset($_SERVER['PATH_INFO']) OR $_SERVER['PATH_INFO'] = '/';
 set_include_path(get_include_path() . PATH_SEPARATOR . realpath(BASE_FILEPATH));
 set_include_path(get_include_path() . PATH_SEPARATOR . realpath(BASE_FILEPATH. '/system'));
 
-//include BASE_FILEPATH . '/system/autoload.php';
-
 unset($dir);
+
+use VanDragt\Carbon;
--- tests/CacheTest.php
+++ tests/CacheTest.php
@@ -4,18 +4,18 @@ class CacheTest extends PHPUnit_Framework_TestCase {
 
 
 	public function test_has_cacheable_page_request() {
-		$request = Cache::has_cacheable_page_request();
+		$request = Carbon\Cache::has_cacheable_page_request();
 		$this->assertEquals(false, $request);
-		Cache::start();
-		$request = Cache::has_cacheable_page_request();
+		Carbon\Cache::start();
+		$request = Carbon\Cache::has_cacheable_page_request();
 		$this->assertEquals(true, $request);
-		Cache::end();
+		Carbon\Cache::end();
 	}
 
 	public function test_cache_file_from_url() {
-		$path = Cache::cache_file_from_url();
+		$path = Carbon\Cache::cache_file_from_url();
 		$this->assertEquals(true, $path);
-		$path = Cache::cache_file_from_url('/index');
+		$path = Carbon\Cache::cache_file_from_url('/index');
 		$this->assertEquals(true, $path);
 	}
 
