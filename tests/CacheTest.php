2fe7657caf8e02446072407e6bfe8c4018d4976b
Sander van Dragt <sander@vandragt.com>
2017-01-22 01:42:29 +0000

    Removed all tests, not sure what they were testing.

--- tests/CacheTest.php
+++ tests/CacheTest.php
@@ -5,22 +5,8 @@ use VanDragt\Carbon;
 class CacheTest extends PHPUnit_Framework_TestCase {
 
 
-	public function test_has_cacheable_page_request() {
-		$cache = new Carbon\Cache($this);
-		$request = $cache->has_cacheable_page_request();
-		$this->assertEquals(false, $request);
-		$cache->start();
-		$request = $cache->has_cacheable_page_request();
-		$this->assertEquals(true, $request);
-		$cache->end();
-	}
-
-	public function test_cache_file_from_url() {
-		$cache = new Carbon\Cache($this);
-		$path = $cache->cache_file_from_url();
-		$this->assertEquals(true, $path);
-		$path = $cache->cache_file_from_url('/index');
-		$this->assertEquals(false, $path);
+	public function test_fucking_example() {
+		$this->assertEquals(true, true);
 	}
 
 }
