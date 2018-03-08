2f6e946f937db59115e70915a557d80a0ea6179f
Sander van Dragt <sander@vandragt.com>
2018-03-08 14:34:17 +0000

    Remove configuration

--- src/Configuration.php
+++ src/Configuration.php
@@ -1,15 +1,21 @@
-<?php if (!defined('BASE_FILEPATH'))
+<?php
+
+use VanDragt\Carbon;
+
+if (!defined('BASE_FILEPATH'))
 {
 	exit('No direct script access allowed');
 }
 
-class Configuration extends VanDragt\Carbon\Defaults
+class Configuration extends Carbon\Defaults
 {
 
 	const SITE_TITLE = 'Your site';
 	const SITE_MOTTO = 'Example theme of <a href="http://carbon.vandragt.com/">Carbon</a>, the hackable performant (semi) static blogging system.';
 	const CACHE_ENABLED = FALSE;
 
-	const INDEX_PAGE = '';
-	// const ADMIN_PASSWORD = "carbon";
+	// const INDEX_PAGE     = '/index.php';
+
+    // support port forwarding where local HTTP_HOST is different from developer (eg vagrant)
+    // const SERVER_HTTP_HOST = 'localhost';
 }
