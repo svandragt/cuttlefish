99d3b54420cd7fb4e6b61af2182f729e9df9ffab
Sander van Dragt <sander@vandragt.com>
2018-08-03 23:10:21 +0100

    Template view docs

--- src/themes/basic/views/archive.php
+++ src/themes/basic/views/archive.php
@@ -1,11 +1,15 @@
-<?php if ( ! defined( 'BASE_FILEPATH' ) ) {
+<?php
+if ( ! defined( 'BASE_FILEPATH' ) ) {
 	exit( 'No direct script access allowed' );
 }
+?>
 
-echo '<h2>Archive</h2><dl>';
+<h2>Archive</h2>
+<dl>
 
-foreach ( $this->contents as $post ):
+	<?php foreach ( $this->contents as $post ):
 	printf( "<dt>%s</dt><dd><a href='%s'>%s</a></h2></dd>", $post->metadata->Published, $post->link, $post->content->title );
 endforeach;
+	?>
 
-echo '</dl>';
+</dl>
--- src/themes/basic/views/layout.php
+++ src/themes/basic/views/layout.php
@@ -1,4 +1,5 @@
-<?php  if ( ! defined( 'BASE_FILEPATH' ) ) {
+<?php
+if ( ! defined( 'BASE_FILEPATH' ) ) {
 	exit( 'No direct script access allowed' );
 }
 
