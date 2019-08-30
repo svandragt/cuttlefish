88b175e3288ec752fd1cfaee1a4e7d8e201bb0a5
Sander van Dragt <sander@vandragt.com>
2019-08-30 07:04:21 +0100

    Simplified filenaming for logging web errors to carbon.log

--- src/system/Log.php
+++ src/system/Log.php
@@ -2,44 +2,46 @@
 
 namespace VanDragt\Carbon;
 
+use Configuration;
+
 if ( ! defined( 'BASE_FILEPATH' ) ) {
-	exit( 'No direct script access allowed' );
+    exit( 'No direct script access allowed' );
 }
 
 class Log {
 
-	const FILENAME_TEMPLATE = '/carbon-%s.log';
+    const FILENAME_TEMPLATE = '/carbon.log';
 
 
-	static function error( $message ) {
-		error_log( $message, 0 );
-		echo( $message . "<br>" );
-	}
+    static function error($message ) {
+        error_log( $message, 0 );
+        echo( $message . "<br>" );
+    }
 
 
-	static function debug( $message ) {
-		if ( \Configuration::DEBUG_ENABLED ) {
-			$message = sprintf( "[%s] (%s) DEBUG %s", date( 'd/M/Y:H:i:s' ), pathinfo( $_SERVER['PHP_SELF'], PATHINFO_FILENAME ), $message . PHP_EOL );
-			error_log( $message, 3, \Configuration::LOGS_FOLDER . self::filename_per_request() );
-		}
-	}
+    static function debug($message ) {
+        if (Configuration::DEBUG_ENABLED) {
+            $message = sprintf( "[%s] (%s) DEBUG %s", date( 'd/M/Y:H:i:s' ), pathinfo( $_SERVER['PHP_SELF'], PATHINFO_FILENAME ), $message . PHP_EOL );
+            error_log($message, 3, Configuration::LOGS_FOLDER . self::filename_per_request());
+        }
+    }
 
-	static function filename_per_request() {
-		return sprintf( self::FILENAME_TEMPLATE, date( "Y-m-d_H-m-s" ) );
-	}
+    static function filename_per_request() {
+        return sprintf( self::FILENAME_TEMPLATE, date( "Y-m-d_H-m-s" ) );
+    }
 
-	static function info( $message ) {
-		$message = sprintf( "[%s] (%s) INFO %s", date( 'd/M/Y:H:i:s' ), pathinfo( $_SERVER['PHP_SELF'], PATHINFO_FILENAME ), $message . PHP_EOL );
-		error_log( $message, 3, \Configuration::LOGS_FOLDER . self::filename() );
-	}
+    static function info($message ) {
+        $message = sprintf( "[%s] (%s) INFO %s", date( 'd/M/Y:H:i:s' ), pathinfo( $_SERVER['PHP_SELF'], PATHINFO_FILENAME ), $message . PHP_EOL );
+        error_log($message, 3, Configuration::LOGS_FOLDER . self::filename());
+    }
 
-	static function filename() {
-		return sprintf( self::FILENAME_TEMPLATE, date( "Y-m-d" ) );
-	}
+    static function filename() {
+        return self::FILENAME_TEMPLATE;
+    }
 
-	static function warn( $message ) {
-		$message = sprintf( "[%s] (%s) WARN %s", date( 'd/M/Y:H:i:s' ), pathinfo( $_SERVER['PHP_SELF'], PATHINFO_FILENAME ), $message . PHP_EOL );
-		error_log( $message, 3, \Configuration::LOGS_FOLDER . self::filename() );
-	}
+    static function warn($message ) {
+        $message = sprintf( "[%s] (%s) WARN %s", date( 'd/M/Y:H:i:s' ), pathinfo( $_SERVER['PHP_SELF'], PATHINFO_FILENAME ), $message . PHP_EOL );
+        error_log($message, 3, Configuration::LOGS_FOLDER . self::filename());
+    }
 
 }
