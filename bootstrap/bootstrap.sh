c218084e06b7dc8a47d9a5730e19b83620fa9d0a
Sander van Dragt <sander@vandragt.com>
2019-11-04 17:45:07 +0000

    Feeds only generate if the xml extension is installed.

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -6,14 +6,13 @@ add-apt-repository -y ppa:nginx/stable
 
 apt-get -y update
 apt-get -y install nginx zip
-apt-get -y install php7.3-cli php7.3-fpm php7.3-xdebug
+apt-get -y install php7.3-cli php7.3-fpm php7.3-xdebug php7.3-xml
 
 apt-get clean && apt-get -y autoremove &
-
 
 curl -Ss https://getcomposer.org/installer | php
 mv composer.phar /usr/bin/composer
 pushd /vagrant
-  sudo -u vagrant -H composer install &
-  cp src/Configuration-sample.php src/Configuration.php -n
+sudo -u vagrant -H composer install &
+cp src/Configuration-sample.php src/Configuration.php -n
 popd
