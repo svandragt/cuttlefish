e5d01148c3bbec15a609e8ba27b4453dd2b68853
Sander van Dragt <sander@vandragt.com>
2020-03-06 15:22:57 +0000

    Fix switch -> break issue by removing break statements.

--- src/system/File.php
+++ src/system/File.php
@@ -38,11 +38,8 @@ class File
         switch ($this->ext) {
             case 'css': // php cannot detect css
                 return "text/css";
-                break;
-
             default:
                 return $this->getMimetypeFromFile($this->path);
-                break;
         }
     }
 
