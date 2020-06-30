71b2f9a5da8ffddbf346a97ca04553a0ff90c93e
Sander van Dragt <sander@vandragt.com>
2020-06-30 18:56:02 +0100

    Split blog and content off from system files.

--- src/application/functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (! defined('BASE_FILEPATH')) {
-    exit('No direct script access allowed');
-}
-
-// put functions specific to your application here. You can call these from the view templates.
-// Todo is this actually working?
--- src/themes/basic/views/errors.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-// Error pages use page layout
-include __DIR__ . DIRECTORY_SEPARATOR . "pages.php";
