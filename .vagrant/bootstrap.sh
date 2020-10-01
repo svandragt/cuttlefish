e29b5c19b711a58fbe1c39e57c121e4fbd21b1c5
Sander van Dragt <sander@vandragt.com>
2020-10-01 16:46:19 +0100

    Vagrant reinstated.

--- .vagrant/bootstrap.sh
+++ .vagrant/bootstrap.sh
@@ -6,14 +6,12 @@ add-apt-repository -y ppa:nginx/stable
 
 apt-get -y update
 apt-get -y install nginx zip
-apt-get -y install php7.3-cli php7.3-fpm php7.3-xdebug
+apt-get -y install php7.3-cli php7.3-fpm php7.3-xdebug php7.3-curl
 
 apt-get clean && apt-get -y autoremove &
 
-
 curl -Ss https://getcomposer.org/installer | php
 mv composer.phar /usr/bin/composer
-pushd /vagrant
-  sudo -u vagrant -H composer install &
-  cp src/Configuration-sample.php src/Configuration.php -n
-popd
+pushd /vagrant || exit
+  sudo -u vagrant -H composer install
+popd || exit
