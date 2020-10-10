98838fbd160ef8eaaedf6e3da379eee4442af43a
Sander van Dragt <sander@vandragt.com>
2020-10-10 11:43:09 +0100

    highlight readiness.

--- .vagrant/start.sh
+++ .vagrant/start.sh
@@ -6,5 +6,5 @@ sudo -u www-data mkdir /srv/app/{_logs,_cache,content} -p
 sudo cp /vagrant/.vagrant/etc/ / -r
 sudo service php7.4-fpm restart
 sudo service nginx restart
-
-echo "Ready. http://cuttlefish.test"
+echo ""
+echo "🐡  Ready. http://cuttlefish.test"
